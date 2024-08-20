b48c9b1ed926fac62468efe43be7b6b12d503dd2
robinkenneth <robinkenneth@daremigtythings.bbrouter>
2024-08-21 01:44:53 +0800

    Initial Project Commit
    - add maven spring boot dependencies
    
    Change-Id: I06347c5c5ca41416de3e23927abe0667428693ae

---
+++ src/main/java/com/mycourier/MycourierApplication.java
@@ -0,0 +1,13 @@
+package com.mycourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycourierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycourier/MycourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
